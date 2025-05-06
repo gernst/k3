1fbda219283b02f3848edeec1a4bb419b7f201bd
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2025-05-06 16:28:36 -0400

    much progress

--- python/parse.py
+++ python/parse.py
@@ -5,10 +5,12 @@ import pysmt
 from sexpr import *
 from syntax import *
 
+
 def parse(cmds):
     parser = Parser()
     cmds_ = [parser.command(cmd) for cmd in cmds]
     return cmds_
+
 
 class Parser:
     def __init__(self):
@@ -47,31 +49,100 @@ class Parser:
                 decl_ = self.sorts[name]
                 return self.type_manager.get_type_instance(decl_, *args_)
 
-    def expression(self, scope, sexpr):
+    def formal(self, name, sort):
+        sort_ = self.type(sort)
+        var_ = self.formula_manager.Symbol(name, sort_)
+        return var_
+
+    def formals(self, sexpr):
+        return [self.formal(sym.name, sort) for sym, sort in sexpr]
+
+    def formula(self, scope, sexpr):
+        term_ = self.term(scope, sexpr)
+        assert term_.get_type().is_bool_type()
+        return term_
+
+    def assign(self, scope, name, rhs):
+        var_ = scope[name]
+        rhs_ = self.term(scope, rhs)
+        assert var_.get_type() == rhs_.get_type()
+        return (var_, rhs_)
+
+    def term(self, scope, sexpr):
         match sexpr:
+            case Numeral(value):
+                return self.formula_manager.Int(int(value))
+
             case Symbol("true"):
-                pass
+                return self.formula_manager.TRUE()
 
             case Symbol(name) if name in scope:
                 return scope[name]
 
             case _:
-                raise ValueError("Not an expression: " + str(sexpr))
-
+                raise ValueError("Not an term: " + str(sexpr))
+    
     def statement(self, scope, sexpr):
         match sexpr:
-            case ("sequence", *statements):
+            case (Symbol("!"), body, *attributes):
+                body_ = self.statement(scope, body)
+                body_.attributes += attributes
+                return body_
+
+            case (Symbol("return"),):
+                return Return()
+
+            case (Symbol("break"),):
+                return Break()
+
+            case (Symbol("continue"),):
+                return Continue()
+
+            case (Symbol("assume"), arg):
+                arg_ = self.formula(scope, arg)
+                return Assume(arg_)
+
+            case (Symbol("assign"), pairs):
+                pairs_ = [ self.assign(scope, sym.name, rhs) for sym, rhs in pairs ]
+                return Assign(pairs_)
+
+            case (Symbol("havoc"), vars):
+                vars_ = [ scope[sym.name] for sym in vars ]
+                return Havoc(vars_)
+
+            case (Symbol("label"), Symbol(name)):
+                return Label(name)
+
+            case (Symbol("goto"), Symbol(name)):
+                return Goto(name)
+
+            case (Symbol("if"), condition, iftrue):
+                condition_ = self.formula(scope, condition)
+                iftrue_ = self.statement(scope, iftrue)
+                return If(condition_, iftrue_)
+
+            case (Symbol("if"), condition, iftrue, iffalse):
+                condition_ = self.formula(scope, condition)
+                iftrue_ = self.statement(scope, iftrue)
+                iffalse_ = self.statement(scope, iffalse)
+                return If(condition_, iftrue_, iffalse)
+
+            case (Symbol("while"), condition, body):
+                condition_ = self.formula(scope, condition)
+                body_ = self.statement(scope, body)
+                return While(condition_, body_)
+
+            case (Symbol("sequence"), *statements):
                 statements_ = [
                     self.statement(scope, statement) for statement in statements
                 ]
                 return Sequence(statements_)
 
-            case ("assume", arg):
-                arg_ = expression(self, scope, arg)
-                return Assume(arg_)
-
-            case ("return",):
-                return Return()
+            case (Symbol("choice"), *statements):
+                statements_ = [
+                    self.statement(scope, statement) for statement in statements
+                ]
+                return Choice(statements_)
 
             case _:
                 raise ValueError("Not a statement: " + str(sexpr))
@@ -85,7 +156,7 @@ class Parser:
                 return pysmt.smtlib.script.SmtLibCommand("declare-sort", [decl_])
 
             case (Symbol("assert"), expr):
-                expr_ = self.expression({}, expr)
+                expr_ = self.formula({}, expr)
                 print(expr_)
 
             case (Symbol("declare-var"), Symbol(name), sort):
@@ -95,9 +166,21 @@ class Parser:
                 self.globals[name] = var_
                 return DeclareVar(var_)
 
-            case (Symbol("declare-proc", Symbol(name), inputs, outputs, locals)):
+            case (Symbol("define-proc"), Symbol(name), inputs, outputs, locals, body):
+                formals = inputs + outputs + locals
+                names_ = [sym.name for sym, sort in formals]
+                assert len(names_) == len(set(names_))
 
-                pass
+                inputs_ = self.formals(inputs)
+                outputs_ = self.formals(outputs)
+                locals_ = self.formals(locals)
+                vars_ = inputs_ + outputs_ + locals_
+
+                scope_ = self.globals | dict(zip(names_, vars_))
+                body_ = self.statement(scope_, body)
+
+                proc_ = Procedure(name, inputs_, outputs_, locals_, body_)
+                return DeclareProc(proc_)
 
             case _:
                 raise ValueError("Not a command: " + str(sexpr))
--- python/syntax.py
+++ python/syntax.py
@@ -1,10 +1,87 @@
 from dataclasses import dataclass
 
-import pysmt
+
+class Statement:
+    attributes = dict()
+    pass
+
+
+@dataclass
+class Return(Statement):
+    pass
+
+
+@dataclass
+class Break(Statement):
+    pass
+
+
+@dataclass
+class Continue(Statement):
+    pass
+
+
+@dataclass
+class Assume(Statement):
+    formula: object
+
+
+@dataclass
+class Assign(Statement):
+    pairs: list[tuple[object, object]]
+
+
+@dataclass
+class Label(Statement):
+    name: str
+
+
+@dataclass
+class Goto(Statement):
+    name: str
+
+
+@dataclass
+class If(Statement):
+    condition: object
+    iftrue: Statement
+    iffalse: Statement
+
+
+@dataclass
+class While(Statement):
+    condition: object
+    body: Statement
+
+
+@dataclass
+class Sequence(Statement):
+    statements: list[Statement]
+
+
+@dataclass
+class Choice(Statement):
+    statements: list[Statement]
+
+
+@dataclass
+class Procedure:
+    name: str
+    inputs: list[object]
+    outputs: list[object]
+    locals: list[object]
+    body: Statement
+
 
 class Command:
     pass
 
+
 @dataclass
 class DeclareVar(Command):
-    var: pysmt.fnode.FNode
+    var: object
+
+
+@dataclass
+class DeclareProc(Command):
+    proc: Procedure
